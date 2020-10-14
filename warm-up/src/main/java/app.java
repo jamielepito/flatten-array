2baca631160967d2a5e355161cbf6449ed292171
Dennis Ortega <dortega2@wisc.edu>
2020-10-14 10:17:08 -0500

    my commit

--- warm-up/src/main/java/app.java
+++ warm-up/src/main/java/app.java
@@ -1,5 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class app {
-    public static void main(String[] args) {
-        
+    public static <E> void main(String[] args) {
+//        E [] arr  = (E[]) new String []{"a", "b", null, "c"};
+        E[][] arr2 = (E[][]) new String[][]{
+                {"a", "b", null, "c"},
+                {"d", null},
+                {null, "e"},
+                {"f"}};
+
+        List<E> flatList = flattenArray(arr2);
+        System.out.println(flatList);
+
+        E[][] arrInt = (E[][]) new Integer[][]{
+                {1, 2, null, 3},
+                {4, null},
+                {null, 5},
+                {6}};
+
+        List<E> flatList2 = flattenArray(arrInt);
+        System.out.println(flatList2);
+    }
+
+    public static <E> List<E> flattenArray(E[][] someArr) {
+        ArrayList<E> flatList = new ArrayList<>();
+
+        for (int i = 0; i < someArr.length; i++) {
+            for(int j = 0; j < someArr[i].length ; j++) {
+                if (someArr[i][j] != null) {
+                    flatList.add(someArr[i][j]);
+                }
+            }
+        }
+
+        return flatList;
     }
 }
